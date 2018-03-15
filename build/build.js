6eca16c061c83cb681b64d64fd7ea0825ea8ed08
duxingyu <duxy1995@163.com>
2018-03-15 15:07:39 +0800

    更新npm包及配置文件

--- build/build.js
+++ build/build.js
@@ -1,39 +1,45 @@
-/* 用于生成环境构建并在命令行输出相关信息 */
+'use strict';
+require('./check-versions')();
 
-require('./check-versions')()
+process.env.NODE_ENV = 'production';
 
-process.env.NODE_ENV = 'production'
+const ora = require('ora');
+const rm = require('rimraf');
+const path = require('path');
+const chalk = require('chalk');
+const webpack = require('webpack');
+const config = require('../config');
+const webpackConfig = require('./webpack.prod.conf');
 
-var ora = require('ora')
-var rm = require('rimraf')
-var path = require('path')
-var chalk = require('chalk')
-var webpack = require('webpack')
-var config = require('../config')
-var webpackConfig = require('./webpack.prod.conf')
+const spinner = ora('building for production...');
+spinner.start();
 
-// 命令行输出
-var spinner = ora('building for production...')
-spinner.start()
-
-// 删除原有dist目录下的static目录
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err
-  webpack(webpackConfig, function(err, stats) {
-    spinner.stop()
-    if (err) throw err
-    process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false
-    }) + '\n\n')
+  if (err) throw err;
+  webpack(webpackConfig, (err, stats) => {
+    spinner.stop();
+    if (err) throw err;
+    process.stdout.write(
+      stats.toString({
+        colors: true,
+        modules: false,
+        children: false, // if you are using ts-loader, setting this to true will make typescript errors show up during build
+        chunks: false,
+        chunkModules: false,
+      }) + '\n\n'
+    );
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
-  })
-})
+    if (stats.hasErrors()) {
+      console.log(chalk.red('  Build failed with errors.\n'));
+      process.exit(1);
+    }
+
+    console.log(chalk.cyan('  Build complete.\n'));
+    console.log(
+      chalk.yellow(
+        '  Tip: built files are meant to be served over an HTTP server.\n' +
+          "  Opening index.html over file:// won't work.\n"
+      )
+    );
+  });
+});
