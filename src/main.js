b0de2ac0823eb03814bcd26b77e54f31d7fe02aa
duxingyu <duxy1995@163.com>
2018-03-18 22:33:58 +0800

    加入element-ui组件

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import Vue from 'vue';
 import axios from 'axios';
+import { Pagination } from 'element-ui';
 import router from './router/router';
 import store from './store/index';
 import App from './App';
 
+// reset、公共css、markdown样式
 import './assets/css/reset.css';
 import './assets/css/common.css';
 import './assets/css/github-markdown.css';
+import './assets/el.scss';
 
 Vue.prototype.$http = axios;
+Vue.use(Pagination);
 
 Vue.config.productionTip = false;
 
