a734bc5bebcc763690c9a7c67ae26e5555f19f3c
duxingyu <duxy1995@163.com>
2018-03-20 17:16:04 +0800

    使用element-ui的Loading代替loading图

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
 import Vue from 'vue';
 import axios from 'axios';
-import { Pagination } from 'element-ui';
+import { Pagination, Select, Option } from 'element-ui';
 import router from './router/router';
 import store from './store/index';
 import App from './App';
 
-// reset、公共css、markdown样式
+// reset、公共css、markdown样式、element-ui主题色
 import './assets/css/reset.css';
 import './assets/css/common.css';
 import './assets/css/github-markdown.css';
 import './assets/el.scss';
 
+axios.defaults.baseURL = 'https://cnodejs.org/api/v1/';
 Vue.prototype.$http = axios;
+// element-ui的Loading组件配置
+Vue.prototype.$loadConfig = {
+  target: '#app',
+  fullscreen: false,
+  lock: true,
+};
 Vue.use(Pagination);
+Vue.use(Select);
+Vue.use(Option);
 
 Vue.config.productionTip = false;
 
